ea4f5095e6a9af3ea0b7a545ce1d2d3d29efae83
Kenny Gonsalves <gonsalveskenny@gmail.com>
2022-12-15 10:56:37 +0000

    Intial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/kenny', (req, res) => {
+  res.send('Hello Kenny!')
+})
+
+app.get('/jeston', (req, res) => {
+  res.send('Hello Jeston!')
+})
+
+app.get('/brothers', (req, res) => {
+  res.send('Hello KenJas!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
