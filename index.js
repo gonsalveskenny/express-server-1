afa2dbaf9a6fe724df5f592924beb8b0cce125a1
Kenny Gonsalves <gonsalveskenny@gmail.com>
2022-12-16 13:28:12 +0000

    Modified

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/update', (req, res) => {
 })
 
 app.get('/release', (req, res) => {
-  res.send('Hello to Latest Release-1!')
+  res.send('Hello to Latest Release-2!')
 })
 
 app.listen(port, () => {
